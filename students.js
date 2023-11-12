3eb1ac32f9049bd8f7c67a6957e96de81d7d64f8
Svetlana <luchiana.sv@gmail.com>
2023-11-12 13:33:13 +0100

    created script get the best student - hw 3.2.2

---
+++ students.js
@@ -0,0 +1,61 @@
+const studentGroups = [
+  [
+    { name: "Ivan", score: 35, date: "2022-10-11" },
+    { name: "Maria", score: 5, date: "2022-10-10" },
+    { name: "Olga", score: 0, date: "" },
+    { name: "Stepan", score: 35, date: "2022-10-12" },
+    { name: "Oleg", score: 9, date: "2022-10-01" },
+    { name: "Zanna", score: 15, date: "2022-10-11" },
+  ],
+  [
+    { name: "Margo", score: 0, date: "2022-10-11" },
+    { name: "Natalia", score: 25, date: "2022-10-10" },
+    { name: "Marina", score: 25, date: "2022-10-01" },
+    { name: "Dmitry", score: 25, date: "2022-10-12" },
+    { name: "Denis", score: 0, date: "2022-10-02" },
+    { name: "Vadimyr", score: 1, date: "2022-10-11" },
+  ],
+  [
+    { name: "Irina", score: 0, date: "2022-10-11" },
+    { name: "Vasily", score: 0, date: "2022-10-10" },
+    { name: "David", score: 0, date: "2022-10-11" },
+    { name: "Kristina", score: 0, date: "2022-10-12" },
+    { name: "Varvara", score: 0, date: "2022-10-01" },
+    { name: "Tanya", score: 0, date: "2022-10-11" },
+  ],
+];
+
+function getTheBestStudent(groups) {
+  const allScores = [];
+  let bestScore;
+
+  const studentWithBestScore = [];
+  studentGroups.forEach((group) => {
+    group.forEach((student) => {
+      if (typeof student.score === "number" && student.score > 0) {
+        allScores.push(student.score);
+      }
+    });
+  });
+  bestScore = Math.max(...allScores);
+  studentGroups.forEach((group) => {
+    group.forEach((student) => {
+      if (student.score === bestScore) {
+        studentWithBestScore.push(student);
+      }
+    });
+  });
+  if (studentWithBestScore.length !== 0) {
+    let earliestDate = new Date();
+    let bestStudent;
+
+    studentWithBestScore.forEach((student) => {
+      if (new Date(student.date) < earliestDate) {
+        earliestDate = new Date(student.date);
+        bestStudent = student;
+      }
+    });
+    console.log(`Congratulation to the best student ${bestStudent.name}!`);
+  }
+}
+getTheBestStudent(studentGroups);
